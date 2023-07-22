a59f4136d9a9fa84769a31ab9664f9f4a5e53ac1
Oghenetega Etareh <tega.etare@gmail.com>
2023-07-21 20:25:25 -0400

    Setting Up the Table

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import ContactList from './components/ContactList'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,7 +8,7 @@ function App() {
   return (
     
       <div>
-       
+        <ContactList />
       </div>
      
     
